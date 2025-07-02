0866c0657c08345ecef791e778cae2b508e3c961
Likith Kumar Suresh(UST <294109@ust.com>
2025-07-02 12:52:18 +0530

    Routes+links

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter,Route, Routes } from "react-router-dom"
 import About from "./pages/About"
 import Home from "./pages/Home"
+import Navbar from "./componants/Navbar"
  
 function App() {
   
 
   return (
+    
     <BrowserRouter>
+
+    <Navbar></Navbar>
     <Routes>
+      
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/about" element={<About></About>}></Route>
 
