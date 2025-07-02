1ccaac36ac0cbf5c0ea954f21778d1f5032304e2
Likith Kumar Suresh(UST <294109@ust.com>
2025-07-02 11:33:42 +0530

    UseReducer hook1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,12 +4,25 @@ import Coco from "./componants/Coco"
 import ElementVar from "./componants/ElementVar"
 import Counter from "./componants/Counter"
 import Forms from "./componants/Forms"
+import { useReducer } from "react"
  
-function App() {
-
+function reducer(state, action){
+  if(action.type=="inc"){
+    return state+1;
+  }
+  return state;
   
+}
+function App() {
+  const [state ,dispatch]=useReducer(reducer,0)
+
   return (
-    <Forms></Forms>
+    <div>
+          <h1>count :{state}</h1>
+    <button onClick={()=>dispatch({type:"inc",payload:1})}>inc</button>
+    </div>
+     
+
   )
 }
 
