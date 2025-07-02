e0c5e94f28d932eb384abdd260a5cf504bc5c2dd
Likith Kumar Suresh(UST <294109@ust.com>
2025-07-02 12:43:31 +0530

    Routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,29 +1,19 @@
-
-import { useReducer, useState } from "react"
+import { BrowserRouter,Route, Routes } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
  
 function App() {
-  const [state ,dispatch]=useReducer(reducer,{balance:0})
-  const [amount,setAmount]=useState(0)
-
-  function reducer(state, action){
-    if(action.type=="add"){
-      return {...state,balance:state.balance+action.payload}
-    }
-    if(action.type=="del"){
-      return {...state,balance:state.balance-action.payload}
-    }
-    return state;
   
-}
 
   return (
-    <div>
-      <h1> Balance:{ state.balance}</h1>
-      <input type="number"
-       value={amount} onChange={(e)=>setAmount(e.target.value)}></input>
-       <button onClick={()=>dispatch({type:"add",payload:Number(amount)})}>Deposit</button>
-       <button onClick={()=>dispatch({type:"del",payload:Number(amount)})}>WithDrawl</button>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
+
+    </Routes>
+    </BrowserRouter>
+
         
 
   )
